b35c45225127cdcce73abeb2921ca64ae1ba593e
Alejo Retamoza <alejoretamoza72@gmail.com>
2023-12-21 12:36:08 -0300

    newsletter

--- src/app/web/layouts/subscription/subscription.component.ts
+++ src/app/web/layouts/subscription/subscription.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { ButtonComponent } from "../../components/button/button.component";
+import { InputComponent } from "../../components/input/input.component";
 
 @Component({
-  selector: 'app-subscription',
-  standalone: true,
-  imports: [],
-  templateUrl: './subscription.component.html',
-  styleUrl: './subscription.component.scss'
+    selector: 'app-subscription',
+    standalone: true,
+    templateUrl: './subscription.component.html',
+    styleUrl: './subscription.component.scss',
+    imports: [NavbarComponent, ButtonComponent, InputComponent]
 })
 export class SubscriptionComponent {
 
