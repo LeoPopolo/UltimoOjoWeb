231691e4ba593b9917e807726fbecc695533bc97
LeoPopolo <leopopologuitarreta@gmail.com>
2023-12-26 12:02:31 -0300

    navbar

--- src/app/admin/layouts/admin/admin.component.ts
+++ src/app/admin/layouts/admin/admin.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-admin',
-  standalone: true,
-  imports: [],
-  templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+    selector: 'app-admin',
+    standalone: true,
+    templateUrl: './admin.component.html',
+    styleUrl: './admin.component.scss',
+    imports: [SidebarComponent]
 })
 export class AdminComponent {
 
