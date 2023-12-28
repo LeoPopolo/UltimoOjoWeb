39afbdbf112ddb9ae9ebbd144024d4fd06957368
LeoPopolo <leopopologuitarreta@gmail.com>
2023-12-28 17:20:18 -0300

    footer created

--- src/app/web/components/navbar/navbar.component.ts
+++ src/app/web/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ RouterModule ],
+  imports: [ RouterModule, CurrencyPipe ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
--- src/app/web/layouts/home/home.component.ts
+++ src/app/web/layouts/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { NewsletterComponent } from "../../components/newsletter/newsletter.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [NavbarComponent, NewsletterComponent]
+    imports: [NavbarComponent, NewsletterComponent, FooterComponent]
 })
 export class HomeComponent {
 
