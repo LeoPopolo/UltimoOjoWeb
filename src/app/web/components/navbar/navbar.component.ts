bd84c4cf6dcc972003a270b46f0fdc78c03ffdf3
LeoPopolo <leopopologuitarreta@gmail.com>
2024-01-04 13:25:56 -0300

    responsive inicio

--- src/app/web/components/navbar/navbar.component.ts
+++ src/app/web/components/navbar/navbar.component.ts
@@ -2,14 +2,19 @@ import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from "../button/button.component";
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss',
-    imports: [RouterModule, CurrencyPipe, ButtonComponent]
+    imports: [RouterModule, CurrencyPipe, ButtonComponent, SidebarComponent]
 })
 export class NavbarComponent {
+  sidebarOpen = false;
 
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
