b048a5bd9bcbc16032f25b00b1c4029805dcb810
Alejo Retamoza <alejoretamoza72@gmail.com>
2024-01-17 13:25:21 -0300

    validaciones de carrito

--- src/app/web/components/navbar/navbar.component.ts
+++ src/app/web/components/navbar/navbar.component.ts
@@ -1,20 +1,35 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ButtonComponent } from "../button/button.component";
-import { SidebarComponent } from "../sidebar/sidebar.component";
+import { ButtonComponent } from '../button/button.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { CartService } from '../../services/cart.service';
+import { ITemplate } from '../../models/template';
 
 @Component({
-    selector: 'app-navbar',
-    standalone: true,
-    templateUrl: './navbar.component.html',
-    styleUrl: './navbar.component.scss',
-    imports: [RouterModule, CurrencyPipe, ButtonComponent, SidebarComponent]
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  imports: [RouterModule, CurrencyPipe, ButtonComponent, SidebarComponent],
 })
 export class NavbarComponent {
   sidebarOpen = false;
+  private readonly cartService = inject(CartService);
+
+  cart = signal<ITemplate[]>([]);
 
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  ngOnInit(): void {
+    this.cartService.getCart().subscribe((data) => {
+      this.cart.set(data);
+    });
+  }
+
+  get total() {
+    return this.cart().reduce((acc, item) => acc + item.price, 0);
   }
 }
--- src/app/web/layouts/pack-template/pack-template.component.ts
+++ src/app/web/layouts/pack-template/pack-template.component.ts
@@ -1,21 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { FooterComponent } from "../../components/footer/footer.component";
-import { NavbarComponent } from "../../components/navbar/navbar.component";
-import { BonusComponent } from "../../components/bonus/bonus.component";
+import { Component, inject, signal } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { BonusComponent } from '../../components/bonus/bonus.component';
 import { ITemplate } from '../../models/template';
-
+import { CartService } from '../../services/cart.service';
 
 @Component({
-    selector: 'app-pack-template',
-    standalone: true,
-    templateUrl: './pack-template.component.html',
-    styleUrl: './pack-template.component.scss',
-    imports: [FooterComponent, NavbarComponent, BonusComponent]
+  selector: 'app-pack-template',
+  standalone: true,
+  templateUrl: './pack-template.component.html',
+  styleUrl: './pack-template.component.scss',
+  imports: [FooterComponent, NavbarComponent, BonusComponent],
 })
 export class PackTemplateComponent {
+  private readonly cartService = inject(CartService);
 
   template = signal<ITemplate>({
     name: 'Pack 20% de descuento',
     price: 76,
   });
+
+  ngOnInit(): void {
+    this.cartService.getCart().subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  addTemplate() {
+    this.cartService.addTemplateToCart(this.template());
+  }
 }
