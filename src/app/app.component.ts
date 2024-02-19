644d5cd382415a70a56cf284c5c7d6cb118acd79
LeoPopolo <leopopologuitarreta@gmail.com>
2024-02-19 16:40:18 -0300

    correccion de etiquetas meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ultimo-ojo';
+export class AppComponent implements OnInit {
+  private readonly meta = inject(Meta):
+
+  ngOnInit(): void {
+    this.meta.updateTag({ name: 'description', content: 'Diseño. arquitectura, plantillas de contrato, decoración' });
+    this.meta.updateTag({ name: 'keywords', content: 'diseño, arquitectura, plantilla, contrato, cuestionario de cliente, presupuesto, decoración, templates, newsletter, design' });
+    this.meta.updateTag({ name: 'robots', content: 'index, follow' });
+    this.meta.updateTag({ 'http-equiv': 'content-language', content: 'es' });
+    this.meta.updateTag({ property: 'og:title', content: 'Último ojo - Diseño y arquitectura' });
+    this.meta.updateTag({ property: 'og:description', content: 'Plantillas de diseño en canva' });
+    this.meta.updateTag({ property: 'og:image', content: './assets/images/logo.png' });
+    this.meta.updateTag({ property: 'og:url', content: 'http://ultimoojo.com' });
+  }
+
 }
